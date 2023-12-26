d72f7cfdaa19b41018ccf438cbcb8fa04af74be2
ABDOUPROGRAMER <67739929+programer238@users.noreply.github.com>
2023-12-26 23:25:13 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,111 @@
+let questions = [
+    {
+        numb: 1,
+        question: "How to write a first order differential equation?",
+        answer: "A.y'(x)+y(x)=F(x)",
+        options: [
+            "A.y'(x)+y(x)=F(x)",
+            "B.y''(x)+y'(x)=F(x)",
+            "C.y'(x)+y''(x)=F(x)",
+            "D.none of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "the integral :∫xy dxdy(0<x<1,1<y<2) equal :",
+        answer: "D.3/4",
+        options: [
+            "A.5/4",
+            "B.0",
+            "C.1",
+            "D.3/4"
+        ]
+    },
+    {
+        numb: 3,
+        question: "we give ∫xy dxdy with (0<x<1,0<y<2x):",
+        answer: "A.Integration begins with Y",
+        options: [
+            "A.Integration begins with Y",
+            "B.Integration begins with X.",
+            "C.doesn't matter which variabl you start with.",
+            "D.i don't know"
+        ]
+    },
+    {
+        numb: 4,
+        question: "the equation of the circle :",
+        answer: "B.(x - x₀)² + (y - y₀)² = R²",
+        options: [
+            "A.(x - x₀)² + (y - y₀)² = 1",
+            "B.(x - x₀)² + (y - y₀)² = R²",
+            "C.x² + y² = 1",
+            "D.x² + y² = R²"
+        ]
+    },
+    {
+        numb: 5,
+        question: "the equation of a ball is :",
+        answer: "B.(x - x₀)² + (y - y₀)² + (z - z₀)² = R²",
+        options: [
+            "A.(x - x₀)² + (y - y₀)² + (z - z₀)² = 1",
+            "B.(x - x₀)² + (y - y₀)² + (z - z₀)² = R²",
+            "C.x² + y² + z² = R²",
+            "D.x² + y² + z² = 1"
+        ]
+    },
+    {
+        numb: 6,
+        question: "the homogeneous solution of:y'(x)+Dy(x)=F(x)",
+        answer: "A.C*e^-∫D",
+        options: [
+            "A.C*e^-∫D",
+            "B.C(x)*e^-∫D",
+            "C.C*e^∫D",
+            "D.none of the above"
+        ]
+    },
+    {
+        numb: 7,
+        question: "The shortest war in history lasted only 38 minutes?",
+        answer: "A.yes",
+        options: [
+            "A.yes",
+            "B.i don't care bro ",
+            "C.no",
+            "D.?"
+        ]
+    },
+    {
+        numb: 8,
+        question: "if delta=0 the homogeneous solution is :",
+        answer: "C.(C1*x+C2)e^rx",
+        options: [
+            "A.C1*e^rx + C2*e^rx",
+            "B.(C1*x+C2*x)e^rx",
+            "C.(C1*x+C2)e^rx",
+            "D.none of the above"
+        ]
+    },
+    {
+        numb: 9,
+        question: "if delta>0 the homogeneous solution is :",
+        answer: "B.C1*e^rx + C2*e^rx",
+        options: [
+            "A.(C1*x+C2*x)e^rx",
+            "B.C1*e^rx + C2*e^rx",
+            "C.C1*e^rx - C2*e^rx",
+            "D.none of the above"
+        ]
+    },
+    {
+        numb: 10,
+        question: "if delta<0 the homogeneous solution is :",
+        answer: "C.e^ax[C1*cos(bx)+C2*sin(bx)]",
+        options: [
+            "A.e^bx[C1*cos(ax)+C2*sin(ax)]",
+            "B.e^ax[C1*cos(bx)-C2*sin(bx)]",
+            "C.e^ax[C1*cos(bx)+C2*sin(bx)]",
+            "D.none of the above"
+        ]
+    }]
